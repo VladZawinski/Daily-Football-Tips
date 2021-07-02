f1c54af8a57d0118ce86a9d48f510f651a8ee3c8
ThihaKaungSet3 <Kaungset138@icloud.com>
2021-07-02 11:21:33 +0630

    change datetime

--- app/src/main/java/com/escatatic/shahadtips/data/model/response/UpdateResultResponse.kt
+++ app/src/main/java/com/escatatic/shahadtips/data/model/response/UpdateResultResponse.kt
@@ -2,8 +2,10 @@ package com.escatatic.shahadtips.data.model.response
 
 import com.escatatic.shahadtips.data.base.BaseResponse
 import com.escatatic.shahadtips.data.model.PickDataModel
+import com.squareup.moshi.Json
 
 data class UpdateResultResponse(
     override val success: Boolean,
+    @field:Json(name = "updatedTip")
     override val data: PickDataModel
 ): BaseResponse<PickDataModel>()
--- app/src/main/java/com/escatatic/shahadtips/extensions/DateExts.kt
+++ app/src/main/java/com/escatatic/shahadtips/extensions/DateExts.kt
@@ -3,13 +3,15 @@ package com.escatatic.shahadtips.extensions
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.format.DateTimeFormatter
+import timber.log.Timber
 
 fun String.changeToAppSpecificDate(): String {
-    val dateTime = DateTime(this)
-    return if (dateTime == DateTime()){
+    val dateTime = DateTime(this).toString(DateTimeFormat.mediumDate())
+
+    return if (dateTime == DateTime().toString(DateTimeFormat.mediumDate())){
         "Today"
     }else {
-        dateTime.toString(DateTimeFormat.mediumDate())
+        dateTime
 //        "${dateTime.dayOfWeek().asText} ${dateTime.dayOfMonth().get()} ${dateTime.monthOfYear().asText}"
     }
 }
